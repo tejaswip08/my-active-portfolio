574fbe321a31736a61e32b71da1598017b66b4c1
tejaswipsagar <tejaswip811@gmail.com>
2025-03-18 23:29:42 +0530

    Second Commit

--- src/components/NavbarProfile.jsx
+++ src/components/NavbarProfile.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card, CardContent } from "@mui/material";
 import Grid from "@mui/material/Grid";
+// import myImage from "../assets/teju.jpg";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBarsStaggered, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const menuBtns = [
   { btn_name: "Home", path: "/" },
@@ -10,8 +14,12 @@ const menuBtns = [
 ];
 
 const NavbarProfile = () => {
-  const menuBtnNavigateMethod = () => {
-    console.log("BTN_TRIGGERING");
+  const [state, setState] = useState({
+    menuBtnEnabled: false,
+  });
+
+  const enableDisableMenu = () => {
+    setState({ menuBtnEnabled: !state.menuBtnEnabled });
   };
 
   return (
@@ -19,35 +27,46 @@ const NavbarProfile = () => {
       <Card className="neumorphic-card" elevation={0}>
         <CardContent>
           <nav>
-            <div className="navbar-div">
-              <div className="navbar-div1">
-                <span className="heading1 font-weight-450">{`<Tejaswi />`}</span>
-              </div>
-              <div className="navbar-div2">
-                {menuBtns.map((item, index) => (
-                  <div key={index}>
-                    <button
-                      className="menu-btn btn-font"
-                      onClick={menuBtnNavigateMethod}
-                    >
-                      {item.btn_name}
-                    </button>
-                  </div>
-                ))}
-              </div>
-            </div>
+            <Grid container spacing={0} sx={{ alignItems: "center" }}>
+              <Grid item lg={4} md={4} sm={12} xs={12}>
+                <div>
+                  <FontAwesomeIcon
+                    className="display-menu-bar"
+                    icon={state.menuBtnEnabled ? faXmark : faBarsStaggered}
+                    size="1x"
+                    onClick={enableDisableMenu}
+                  />
+                  <span className="heading1 font-weight-450">{`<Tejaswi />`}</span>
+                </div>
+              </Grid>
+              <Grid item lg={8} md={8} sm={12} xs={12}>
+                <div
+                  className={
+                    state.menuBtnEnabled ? "navbar-div2" : "hide-navbar-div2"
+                  }
+                >
+                  {menuBtns.map((item, index) => (
+                    <div key={index}>
+                      <button className="menu-btn btn-font">
+                        {item.btn_name}
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              </Grid>
+            </Grid>
           </nav>
         </CardContent>
       </Card>
       <Card className="neumorphic-card" elevation={0} sx={{ mt: 3, p: 15 }}>
         <CardContent>
-          <Grid container spacing={2} sx={{ alignItems: "center" }}>
+          <Grid container spacing={0} sx={{ alignItems: "center" }}>
             <Grid item xs={12} sm={12} md={8} lg={8} sx={{ p: 0 }}>
               <div>
-                <div className="heading3 font-weight-450 line-space">
+                <div className="heading3 font-weight-450 line-space text-shadow">
                   Hello,
                 </div>
-                <div className="heading2 font-weight-600 line-space">
+                <div className="heading2 font-weight-600 line-space text-shadow">
                   I'm a Front End Web Developer
                 </div>
                 <div className="heading4 line-space grey-color">
@@ -84,9 +103,15 @@ const NavbarProfile = () => {
               </div>
             </Grid>
             <Grid item xs={12} sm={12} md={4} lg={4} sx={{ p: 0 }}>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
-              corporis similique impedit minus non ratione exercitationem
-              veritatis fugit! Quasi, harum!
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Voluptates, temporibus accusantium. Quod dolores nam corporis eos
+              dicta amet debitis accusantium, minima cum expedita tempore neque
+              minus iure ipsa animi rem similique quos, earum, iusto itaque vel.
+              Molestias natus deserunt, fugiat eaque accusamus sit explicabo,
+              odit ducimus recusandae id cupiditate similique?
+              {/* <Box className="image-box" sx={{ p: 2 }}>
+                <img className="image-box" src={myImage} />
+              </Box> */}
             </Grid>
           </Grid>
         </CardContent>
